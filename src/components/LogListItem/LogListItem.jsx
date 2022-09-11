3221957e014a069abcb0133bbd8ce8db2818a4a4
abpandanguyen <abpandanguyen@gmail.com>
2022-09-11 13:56:51 -0700

    dates working update rx in log WIP

--- src/components/LogListItem/LogListItem.jsx
+++ src/components/LogListItem/LogListItem.jsx
@@ -1,4 +1,4 @@
-// import './LogListItem.css'
+import './LogListItem.css'
 import { useState } from 'react'
 import LogForm from '../LogForm/LogForm';
 
@@ -10,13 +10,11 @@ export default function LogListItem({ log, handleDelete, updateLogItem, prescrip
             {updateLogStatus ? <LogForm log={log} updateLogItem={updateLogItem} updateLogStatus={updateLogStatus} setUpdateLogStatus={setUpdateLogStatus} prescriptions={prescriptions} /> :
             <div>
                 <button onClick={() => setUpdateLogStatus(!updateLogStatus)}>Update</button>
-                <div>Date: {new Date(log.date).toLocaleDateString()}<span> {log.meridiem} </span></div>            
+                <div>Date: {new Date (log.date).toISOString().split("T")[0]}<span> {log.meridiem} </span></div>            
                 <div>Vitals: {log.vitals}</div>
                 <div>Notes: {log.notes}</div>
                 <div>Prescription: {log.prescriptions.map(p => p.rxName).join(", ")} </div>
                 <button onClick={() => handleDelete(log._id)}>X</button>
-                <button onClick={() => console.log(new Date (log.date).toLocaleDateString())}>Local Date</button>
-                <button onClick={() => console.log(new Date (log.date).toISOString())}>Iso Date</button>
             </div>
             }
         </div>
